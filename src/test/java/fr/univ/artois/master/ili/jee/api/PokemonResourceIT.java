bdcfe5b6a562367de6fd77200582093b21423869
lemorse <arthurr.caron@gmail.com>
2024-02-14 11:12:26 +0100

    :tada: - Initial commit.

---
+++ src/test/java/fr/univ/artois/master/ili/jee/api/PokemonResourceIT.java
@@ -0,0 +1,8 @@
+package fr.univ.artois.master.ili.jee.api;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class PokemonResourceIT extends PokemonResourceTest {
+    // Execute the same tests but in packaged mode.
+}
